00479a93ef28472f73ebf4194634c58ed8b232fd
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2021-09-08 16:27:03 +0200

    [FEATURE] add uebung1.1

---
+++ uebungen/1.1/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'partials/header.php';
+
+require_once 'partials/nav.php';
+
+?>
+
+Hauptseite
+
+<?php
+
+require_once 'partials/footer.php';
+
+?>
---
+++ uebungen/1.1/partials/nav.php
@@ -0,0 +1,13 @@
+Nav
+
+<ul>
+    <li>
+        <a href="index.php">Hauptseite</a>
+    </li>
+    <li>
+        <a href="contact.php">Kontakt</a>
+    </li>
+    <li>
+        <a href="blog.php">Blog</a>
+    </li>
+</ul>
